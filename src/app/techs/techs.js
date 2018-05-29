6f750a931fadef4461ccf96eecf1fbe0b9c52a81
tuanhm <tuanhmse03722@fpt.edu.vn>
2018-05-30 00:16:17 +0700

    tao test

--- src/app/techs/techs.js
+++ src/app/techs/techs.js
@@ -1,11 +1,15 @@
 class TechsController {
   /** @ngInject */
-  constructor($http) {
+  constructor($http, $log) {
+    this.movieData = [];
     $http
-      .get('app/techs/techs.json')
+      .get('http://mds.vtvcab.vn/metadata/delivery/GLOBAL/vod/editorials?filter={"voditem.nodeRefs":"LYS015391000","isVisible":true,"editorial.seriesRef":{"$in":[null,""]},"editorial.Rating.precedence":{"$lte":122},"locale":"vi_VN"}&limit=20&fields=["editorial.Title","editorial.Synopsis","editorial.Rating","editorial.Images","editorial.PromoImages","product.price","editorial.ProgramId","editorial.ServiceLongName","editorial.seriesRef","editorial.episodeNumber","editorial.Year","editorial.ProgrammeStartDate","editorial.ProgrammeEndDate","editorial.Studio","editorial.PrivateMetadata","editorial.Episode"]&sort=[["voditem.period.start",-1],["editorial.ProgrammeStartDate",-1]]')
       .then(response => {
-        this.techs = response.data;
+        // this.techs = response.data;
+        this.movieData = response.data.editorials;
+        $log.log('movieData', this.movieData);
       });
+    $log.log('container', angular.element('.movie-container'));
   }
 }
 
